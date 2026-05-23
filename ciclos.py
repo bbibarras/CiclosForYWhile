42a2f0f6b1f17f974ed71a257e65031f24af692d
barbara <bar.ibarras@duocuc.cl>
2026-05-23 11:14:14 -0400

    Add files via upload

---
+++ ciclos.py
@@ -0,0 +1,28 @@
+for i in range(9):
+    print("No debo botar basura")
+    print(f"{i}) No debo botar basura")
+
+#a=5
+#while(a>0):
+#    print(f"el valor de a es: {a}")
+#    a= int(input("ingrese un valor: "))
+
+
+#arreglo / objeto / sustantivo
+frutas = ["manzana", "peras", "naranjas", "kiwis", "Frutillas", "Mangos"]
+juanitoperez = ["rut", "Nombre", "Direccion", "celular"]
+#recorrer arreglo
+#print(frutas[2])
+#for i in range(6):
+#    print(frutas[i])
+#Se ocupa principalmente para recorrer arreglos
+
+for fruta in frutas:
+    print(f"me gusta comer: {fruta}")
+
+
+#se ocupa principalmente para mantener persistencia/menús
+a=5
+while(a>0):
+    print(f"el valor de a es: {a}")
+    a= int(input("ingrese un valor: "))
